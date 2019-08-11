0195cf2d44ef7af5e54ab227793428537856c5cf
mushiwei <mushiweiit@aliyun.com>
2019-08-11 10:52:05 +0800

    项目初始化

--- src/main.js
+++ src/main.js
@@ -8,3 +8,6 @@ new Vue({
   router,
   render: h => h(App)
 }).$mount('#app')
+
+// node_modules包 public静态资源 src main.js入口模块 router.js路由模块 views路由组件 components非路由组件assets静态资源
+// .browserslistrc 浏览器版本处理 .gitignore忽略管理文件 .editorconfig 代码格式详情
